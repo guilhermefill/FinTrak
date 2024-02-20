c2f04db63acc4035557ca9681c1d1d1384b03187
Guilherme Fill <guilherme.fill@buddegroup.com>
2024-02-20 13:44:54 +0100

    Add routes

--- backend/cmd/web/routes.go
+++ backend/cmd/web/routes.go
@@ -2,6 +2,25 @@ package web
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/guilhermefill/FinTrak/pkg/handlers"
 )
 
-var RoutesRegistrar = func(r *mux.Router) {}
+var RoutesRegistrar = func(r *mux.Router) {
+	r.HandleFunc("/accounts", handlers.GetAccounts).Methods("GET")
+	r.HandleFunc("/accounts/{id}", handlers.GetAccountByID).Methods("GET")
+	r.HandleFunc("/accounts/create", handlers.CreateAccount).Methods("POST")
+
+	r.HandleFunc("/transfers", handlers.GetTransfers).Methods("GET")
+	r.HandleFunc("/transfers/{id}", handlers.GetTransferByID).Methods("GET")
+	r.HandleFunc("/transfers/create", handlers.CreateTransfer).Methods("POST")
+
+	r.HandleFunc("/incomes", handlers.GetIncomes).Methods("GET")
+	r.HandleFunc("/incomes/{id}", handlers.GetIncomeByID).Methods("GET")
+	r.HandleFunc("/incomes/create", handlers.CreateIncome).Methods("POST")
+
+	r.HandleFunc("/expenses", handlers.GetExpenses).Methods("GET")
+	r.HandleFunc("/expenses/{id}", handlers.GetExpenseByID).Methods("GET")
+	r.HandleFunc("/expenses/create", handlers.CreateExpense).Methods("POST")
+	r.HandleFunc("/expenses/categories", handlers.GetCategories).Methods("GET")
+	r.HandleFunc("/expenses/categories/create", handlers.CreateCategory).Methods("POST")
+}
---
+++ backend/pkg/handlers/handlers.go
@@ -0,0 +1,59 @@
+package handlers
+
+import "net/http"
+
+func GetAccounts(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement
+}
+
+func GetAccountByID(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement
+}
+
+func CreateAccount(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement with args
+}
+
+func GetTransfers(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement
+}
+
+func GetTransferByID(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement
+}
+
+func CreateTransfer(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement with args
+}
+
+func GetIncomes(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement
+}
+
+func GetIncomeByID(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement
+}
+
+func CreateIncome(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement with args
+}
+
+func GetExpenses(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement
+}
+
+func GetExpenseByID(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement
+}
+
+func CreateExpense(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement with args
+}
+
+func GetCategories(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement
+}
+
+func CreateCategory(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement with args
+}
